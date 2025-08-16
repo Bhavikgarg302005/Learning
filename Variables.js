3b71c52d587ef5b123e7c860cc22073dde661ea3
bhavikgarg30 <bhavikgarg302005@gmail.com>
2025-08-16 13:01:35 +0530

    main

---
+++ Variables.js
@@ -0,0 +1,11 @@
+const accountId = 144
+let x = 45
+y = 30
+console.table([accountId,x])
+
+
+/*
+ These are the ways you can declare variables
+ const -> constant not changable
+ let -> better way to intialise variables
+*/
---
+++ dataTypes.js
@@ -0,0 +1,9 @@
+let score = 33.34
+
+
+let g = "34.5"
+
+let p = Number(g)
+
+
+console.log(p);
